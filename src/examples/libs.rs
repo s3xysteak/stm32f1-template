83290cf111aafe835f89558a588bd7ad4d0245b9
s3xysteak <s3xysteak@outlook.com>
2025-02-20 17:31:45 +0800

    mods

---
+++ src/examples/libs.rs
@@ -0,0 +1,23 @@
+use stm32f1xx_hal::{pac, prelude::*};
+
+pub fn blink() -> ! {
+    let p = pac::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    let mut flash = p.FLASH.constrain();
+    let rcc = p.RCC.constrain();
+
+    let clock = rcc.cfgr.freeze(&mut flash.acr);
+    let mut delay = cp.SYST.delay(&clock);
+
+    let mut gpioa = p.GPIOA.split();
+
+    let mut led = gpioa.pa0.into_push_pull_output(&mut gpioa.crl);
+
+    loop {
+        led.set_high();
+        delay.delay_ms(1000u16);
+        led.set_low();
+        delay.delay_ms(1000u16);
+    }
+}
